0921750b4e087171aab87d32480f71cc88cfb1f8
achintya chaudhary <achintyac77@gmail.com>
2018-10-19 17:56:01 +0530

    basic functionality working

--- adding.js
+++ adding.js
@@ -1,3 +1,9 @@
 function addfun(){
-	alert("working");	
+		var userInput=document.getElementById("UserInput").value ;
+		var newItem = document.createElement("LI");
+		var textnode = document.createTextNode(userInput);
+		newItem.appendChild(textnode);
+
+		var list=document.getElementById("myList");
+		list.insertBefore(newItem , list.childNodes[0]);
 }

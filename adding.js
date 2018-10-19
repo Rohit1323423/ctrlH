d4c2cb060983c5a0e78719c28c89dd2d45372d74
achintya chaudhary <achintyac77@gmail.com>
2018-10-19 20:14:12 +0530

    deletion

--- adding.js
+++ adding.js
@@ -1,9 +1,12 @@
 function addfun(){
-		var userInput=document.getElementById("UserInput").value ;
-		var newItem = document.createElement("LI");
+		var userInput=document.getElementById("UserInput").value+"<br>" ;
+		var newItem = document.createElement("button");
+		newItem.onclick=function(){
+			console.log("all right");
+			this.remove();
+		}
 		var textnode = document.createTextNode(userInput);
 		newItem.appendChild(textnode);
-
 		var list=document.getElementById("myList");
 		list.insertBefore(newItem , list.childNodes[0]);
 }

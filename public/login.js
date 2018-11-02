1c68ace2cb39ead4fb6cd07b0162a4e3536e5513
achintya chaudhary <achintyac77@gmail.com>
2018-11-03 05:07:20 +0530

    login page working properly

---
+++ public/login.js
@@ -0,0 +1,21 @@
+function login() {
+  email=document.getElementById('username').value;
+  password=document.getElementById('password').value;
+  var db = firebase.firestore();
+  //---------------------------------------//
+  let shopping ;
+  db.collection("users").get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        if(email==doc.data().email && password==doc.data().password){
+            console.log("found data");
+            window.location="home.html";
+            stopping=1;
+          }
+      });
+  });
+  if(shopping==1)
+      window.location="home.html";
+  else
+      document.getElementById('Errorinfo').innerHTML="No Username mapped with this account";
+}

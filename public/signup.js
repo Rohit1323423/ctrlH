ef6498d8f7f3a221fd52b22e361698f3c2308fca
achintya chaudhary <achintyac77@gmail.com>
2018-11-04 01:32:32 +0530

    fixed issue of 'promise'

--- public/signup.js
+++ public/signup.js
@@ -13,22 +13,22 @@ console.log("before " ,stopping);
             stopping=1;
           }
       });
-  });
-  console.log("after " ,stopping);
-  if(stopping===1){
-      alert("already have an account");
+      if(stopping==1){
+          alert("already have an account");
+      }
+      else{
+          alert("registering you in our database");
+          db.collection("users").add({
+              email : email ,
+              password : password
+          })
+          .then(function() {
+              console.log("Document successfully written!");
+          })
+          .catch(function(error) {
+              console.error("Error writing document: ", error);
+          });
+        }
   }
-  if(stopping!=1){
-      alert("registering you in our database");
-      db.collection("users").add({
-          email : email ,
-          password : password
-      })
-      .then(function() {
-          console.log("Document successfully written!");
-      })
-      .catch(function(error) {
-          console.error("Error writing document: ", error);
-      });
-    }
+);
 }

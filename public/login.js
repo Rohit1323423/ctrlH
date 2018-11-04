40c8181b0574147df14a3f54a6fa9666f128a827
achintya chaudhary <achintyac77@gmail.com>
2018-11-04 10:18:16 +0530

    changed shopping to stopping

--- public/login.js
+++ public/login.js
@@ -8,16 +8,17 @@ function login() {
   db.collection("users").get().then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
-        console.log("you are authentic users of firestore");
+        console.log("you are authentic users of firestore ");
         if(email==doc.data().email && password==doc.data().password){
-            stopping=1;
+            console.log(shopping);
+            shopping=1;
           }
       });
-      console.log("found data",shopping);
+
       if(shopping==1)
           window.location="new_home.html";
       else
           document.getElementById('Errorinfo').innerHTML="No Username mapped with this account";
   });
-console.log("after",shopping);
+
 }

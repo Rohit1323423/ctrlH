129231cb36a82b76ed4dff6309e4c280e170ec17
achintya chaudhary <achintyac77@gmail.com>
2018-11-04 08:58:18 +0530

    bug in signin

--- public/login.js
+++ public/login.js
@@ -4,18 +4,20 @@ function login() {
   var db = firebase.firestore();
   //---------------------------------------//
   let shopping=9 ;
+
   db.collection("users").get().then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
+        console.log("you are authentic users");
         if(email==doc.data().email && password==doc.data().password){
-            console.log("found data");
-            window.location="new_home.html";
             stopping=1;
           }
       });
+      console.log("found data",shopping);
+      if(shopping==1)
+          window.location="new_home.html";
+      else
+          document.getElementById('Errorinfo').innerHTML="No Username mapped with this account";
   });
-  if(shopping==1)
-      window.location="new_home.html";
-  else
-      document.getElementById('Errorinfo').innerHTML="No Username mapped with this account";
+console.log("after",shopping);
 }

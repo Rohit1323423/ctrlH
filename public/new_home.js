218b5a6fb058934c9acabdc281aafbe8b0435af9
achintya chaudhary <achintyac77@gmail.com>
2018-11-05 19:25:39 +0530

    now reading data from firestore as well as writing it to firestore

--- public/new_home.js
+++ public/new_home.js
@@ -20,7 +20,7 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
-alert("its me");
+
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
@@ -33,16 +33,44 @@ list.addEventListener('click', function(ev) {
 window.onload=function () {
   console.log("page is loaded first time");
   var db = firebase.firestore();
-  db.collection("users").get().then(function(querySnapshot) {
+
+  db.collection("users").where("email", "==", "piy")
+  .get()
+  .then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log("differnt users registered with us");
-        console.log(doc.data().email);
+        //db.collection("users").doc(doc.id).set
+        db.collection("users").doc(doc.id).collection("timestamp").doc(timestamp).collection("todos").get()
+            .then(function(querySnapshot) {
+                  querySnapshot.forEach(function(doc) {
+                      console.log(doc.data().content);
+                      var li = document.createElement("li");
+                      var inputValue = doc.data().content;
+                      var t = document.createTextNode(inputValue);
+                      li.appendChild(t);
+                      document.getElementById("myList").appendChild(li);
+                      var span = document.createElement("SPAN");
+                      //var txt = document.createTextNode("\u00D7");
+                      var txt = document.createTextNode("Erase");
+                      span.className = "close";
+                      span.appendChild(txt);
+                      li.appendChild(span);
+
+                      for (i = 0; i < close.length; i++) {
+                        close[i].onclick = function() {
+                          var div = this.parentElement;
+                          div.style.display = "none";
+                        }
+                      }
+                  })
+             });
       });
-  }).catch(function(error) {
-    console.log("Error getting document:", error);
-    });
+   })
+  .catch(function(error) {
+    console.log("cannnot find username with ur username");
+  });
 }
+
+
 var date=new Date();
 var month=date.getMonth()+1;
 if(month<10){
@@ -56,13 +84,14 @@ var timestamp=""+date.getFullYear()+month+day;
 
 // Create a new list item when clicking on the "Add" button
 function addfun() {
+  //creating element dynamically to insert
   var li = document.createElement("li");
   var inputValue = document.getElementById("UserInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    //alert("You must write something!");
-  } else {
+    alert("You must write something!");
+  }else {
     document.getElementById("myList").appendChild(li);
   }
   document.getElementById("UserInput").value = "";
@@ -75,18 +104,16 @@ function addfun() {
   .then(function(querySnapshot) {
     querySnapshot.forEach(function(doc) {
       //db.collection("users").doc(doc.id).set
-
       db.collection("users").doc(doc.id).collection("timestamp").doc(timestamp).collection("todos").add({
           checked : "0" ,
           content : inputValue
-      })
+          })
       .then(function() {
           console.log("your current to do went to database");
-      })
+          })
       .catch(function(error) {
           console.error("Error writing document: ", error);
       });
-
       //console.log(doc.id, " => ", doc.id);
   });
   })
@@ -94,7 +121,7 @@ function addfun() {
     console.log("cannnot find username with ur username");
   });
 
-
+// uploading finished
 
   var span = document.createElement("SPAN");
   //var txt = document.createTextNode("\u00D7");
